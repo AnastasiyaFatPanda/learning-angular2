3f964b5fed0ef740318d675cb019a71acc54d848
AnastasiyaFatPanda <kakuraanastasia@gmail.com>
2017-08-25 16:42:37 +0300

    Основы Angular - ChildComponent
    Changed:
    -icon
    -html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,51 +1,44 @@
-import { Component } from '@angular/core';
-     
-export class Item{
+import { Component } from "@angular/core";
+
+export class Item {
     purchase: string;
     done: boolean;
     price: number;
-     
+
     constructor(purchase: string, price: number) {
         this.purchase = purchase;
         this.price = price;
         this.done = false;
     }
-} 
+}
 
-//чтобы определить AppComponent как компонент, необходимо прямо 
+//чтобы определить AppComponent как компонент, необходимо прямо
 //перед его описанием ->
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.css"]
 })
 
-//Чтобы класс мог использоваться в других модулях, 
-//он определяется с ключевым словом export.
-export class AppComponent { 
-   // name = 'Anastasiya';
-   // age = 22;
+export class AppComponent {
+    name: string = "Anastasiya";
+    age: number = 22;
 
-    items: Item[] = 
-    [
+    items: Item[] = [
         { purchase: "Хлеб", done: false, price: 15.9 },
         { purchase: "Масло", done: false, price: 60 },
         { purchase: "Картофель", done: true, price: 22.6 },
-        { purchase: "Сыр", done: false, price:310 }
+        { purchase: "Сыр", done: false, price: 310 }
     ];
 
     addItem(text: string, price: number): void {
-         
-        if(text==null || text==undefined || text.trim()=="")
-            return;
-        if(price==null || price==undefined)
-            return;
+        if (text == null || text == undefined || text.trim() == "") return;
+        if (price == null || price == undefined) return;
         this.items.push(new Item(text, price));
     }
-<<<<<<< HEAD
 
-    // increase() : void {
-    //   this.age++;
-    // }
-=======
+    clicks: number = 0;
+    onChanged(increased) {
+        increased == true ? this.clicks++ : this.clicks--;
+    }
 }
--- src/app/child.component.ts
+++ src/app/child.component.ts
@@ -1,15 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 
-@Component ({
-    selector: 'child',
-    templateUrl: './child.component.html'
+@Component({
+    selector: "child",
+    templateUrl: "./child.component.html"
 })
+export class ChildComponent {
+    //example fir @Input
+    @Input() name: string;
+    @Input() age: number;
 
-export class ChildComponent{
-    name = 'Anastasiya';
-    age = 22; 
+    // increase(): void {
+    //     this._age++;
+    // }
 
-    increase() : void {
-      this.age++;
+    // @Input()
+    // set age(age: number) {
+    //     if (age < 0) {
+    //         this._age = 0;
+    //     } else if (age > 100) {
+    //         this._age = 100;
+    //     } else this._age = age;
+    // }
+
+    // get age(){
+    //     return this._age;
+    // }
+
+    //example fir @Output
+    @Output() onChanged = new EventEmitter<boolean>();
+    change(increased) {
+        this.onChanged.emit(increased);
     }
 }
